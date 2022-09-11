c5f1e40bd67d6795fddf439352bae25fd8dc095a
Dotun <dotun.bee@gmail.com>
2022-09-11 13:49:11 -0700

    edit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
-*main -Entry point
+*main - prints the standard error sandwiched around the standard output
 *
 *Return: Always 1(Success)
 */
 int main(void)
 {
-printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 }
 return (1);
 }
