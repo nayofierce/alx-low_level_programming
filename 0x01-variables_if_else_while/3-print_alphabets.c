8c7cf41346c2647c66bf3f939b723f4218001ad1
Dotun <dotun.bee@gmail.com>
2022-09-11 16:53:49 -0700

    edit 4

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
 /**
-*main -Entry point
+ *main -Entry point
 *
 *Return: Always 0(Success)
 */
 
 int main(void)
-char i;
-char y;
 {
+char i; /*Declaring statement*/
+char y;
+/*for lower case*/
 for (i = 'a'; i = 'z'; i++)
 {
 putchar(i);
 }
+/*for upper case*/
 for (y = 'A'; y = 'Z'; y++)
 {
 putchar (y);
