91c848bd5290c8b305a63dab9a99fa53fa2ae6cc
Dotun <dotun.bee@gmail.com>
2022-09-12 05:34:45 -0700

    edit b

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+char letter;
+for ((letter = 'a'); (letter = 'z'); letter++)
+while (letter == 'q') /*continue if is q*/
+{
+continue;
+}
+while (letter == 'e') /*continue if is q*/
+{
+continue;
+}
+{
+putchar(letter);
+putchar('\n');
+}
+return (0);
+}
