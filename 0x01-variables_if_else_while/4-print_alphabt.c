8f578e82d9293c00c659fb24a2a591ab846eb91d
Dotun <dotun.bee@gmail.com>
2022-09-12 05:53:37 -0700

    edit b

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *main - Entry point
  *
@@ -8,7 +7,7 @@
 int main(void)
 {
 char letter;
-for ((letter = 'a'); (letter = 'z'); letter++)
+for ((letter = 'a'); (letter <= 'z'); letter++)
 while (letter == 'q') /*continue if is q*/
 {
 continue;
