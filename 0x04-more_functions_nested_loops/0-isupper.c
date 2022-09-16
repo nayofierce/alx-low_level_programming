2a70ded4b1b2e7c69f942e7557c91f38dde7e76c
Dotun <dotun.bee@gmail.com>
2022-09-15 19:27:02 -0700

    fix bugs

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,17 +2,11 @@
 
 /**
  * _isupper - Single line, short desription
- * @c: contains value to be compared
- * Return: 1, 0
+ * @c: member
+ * Return: 1(on success) otherwise 0
  */
 
 int _isupper(int c)
 {
-if (c > 'A' && c < 'Z')
-{
-return (1);
-}
-else
-{
-return (0);
+return ((c >= 'A' && c <= 'Z') ? 1 : 0);
 }
