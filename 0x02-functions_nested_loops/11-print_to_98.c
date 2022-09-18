66416f2efab667602b1287049a3a901cabcedfbc
Dotun <dotun.bee@gmail.com>
2022-09-18 16:27:30 -0700

    b

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers between n and 98
+ *@n: member
+ */
+void print_to_98(int n)
+{
+int a;
+for (a = n; a = 98; a++)
+{
+_putchar(a);
+_putchar(',');
+_putchar(' ');
+}
+}
+
