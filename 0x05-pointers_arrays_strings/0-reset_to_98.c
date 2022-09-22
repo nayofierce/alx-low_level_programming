be5314f896e3bae91b5d7d40da4975acfd9e8415
Dotun <dotun.bee@gmail.com>
2022-09-21 18:53:04 -0700

    hi

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of a variable
+ * @n: member
+ */
+void reset_to_98(int *n)
+{
+int a;
+n = &a;
+*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is where we learnt first about c pointers and arrays
