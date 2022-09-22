d12b5fa0aa779b7b8f76c84605e28c60becd2dcf
Dotun Balogun <111025151+nayofierce@users.noreply.github.com>
2022-09-23 00:42:43 +0100

    s

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strcmp - compares two strings
- * @s1:string one
- * @s2: string two
- * Return: int
+ * _strcmp - Compare two strings
+ * @s1: string
+ * @s2: string
+ * Return: negative int if s1 < s2, 0 if matching, and positive int if s1 > s2
  */
 
 int _strcmp(char *s1, char *s2)
