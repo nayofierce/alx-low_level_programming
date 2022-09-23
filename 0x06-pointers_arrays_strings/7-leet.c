b7dfb2ebfdf9aad962af653ae481e1aae6d54cef
Dotun <dotun.bee@gmail.com>
2022-09-23 13:44:51 -0700

    d

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,18 +8,16 @@
  */
 char *leet(char *str)
 {
-int indx1 = 0, indx2;
-char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
-
-while (str[indx1])
+int count = 0, i = 0;
+char *letters = "aeotlAEOTL";
+char *num = "4307143071";
+while (str[count] != '\0')
 {
-for (indx1 = 0; indx1 <= 7; indx2++)
-{
-if (str[indx1] == leet[indx2] ||
-str[indx1] - 32 == leet[indx2])
-str[indx1] = indx2 + '0';
+if (str[count] == letters[i])
+str[count] = num[i];
+i++;
 }
-indx1++;
+count++;
 }
 return (str);
 }
