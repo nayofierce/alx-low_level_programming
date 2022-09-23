8316b4a99618e47465fc2b697a4c0fd1670cd65f
Dotun <dotun.bee@gmail.com>
2022-09-22 17:27:00 -0700

    edit

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,21 +8,22 @@
  */
 char *rot13(char *s)
 {
-	int i, j;
-	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char r[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+int i, j;
+char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char r[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (j = 0; a[j] != '\0'; j++)
-		{
-			if (s[i] == a[j])
-			{
-				s[i] = r[j];
-				break;
-			}
-		}
-	}
-
-	return (s);
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; a[j] != '\0'; j++)
+{
+if (s[i] == a[j])
+{
+s[i] = r[j];
+break;
 }
+}
+}
+
+return (s);
+}
+
