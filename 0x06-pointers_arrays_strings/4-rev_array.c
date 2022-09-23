e96c5e02d468741df46c28936e0b0877f2befe12
Dotun <dotun.bee@gmail.com>
2022-09-22 17:16:21 -0700

    g

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,14 +7,15 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i, j, tmp;
+int i, j, tmp;
 
-	j = n - 1;
+j = n - 1;
 
-	for (i = 0; i < n / 2; i++)
-	{
-		tmp = a[i];
-		a[i] = a[j];
-		a[j--] = tmp;
-	}
+for (i = 0; i < n / 2; i++)
+{
+tmp = a[i];
+a[i] = a[j];
+a[j--] = tmp;
 }
+}
+
